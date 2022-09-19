dfccdd318128c4e3b3e43f4f4a86c02246d84770
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-19 13:26:15 +0200

    Update InterfaceBase.php

--- src/Interfaces/InterfaceBase.php
+++ src/Interfaces/InterfaceBase.php
@@ -2,6 +2,30 @@
 
 namespace Sunhill\Home\Interfaces;
 
-class InterfaceBase 
+/**
+ * Base class for interfaces (e.g. OpenHAB)
+ */
+abstract class InterfaceBase 
 {
+      
+      protected function prepareCommand(string $name)
+      {  
+      }
+  
+      protected function sendCommand(string $name, string $command, $options = null)
+      {
+      }
+
+      public function getSwitch(string $name): bool
+      {
+          $this->prepareCommand($name);
+          return $this->sendCommand($name,'getLight');          
+      }
+  
+      public function setSwitch(string $name, bool $on)
+      {
+          $this->prepareCommand($name);
+          return $this->sendCommand($name,'getLight',$on);          
+      }  
+
 }  
