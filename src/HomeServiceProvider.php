cbdbe274f1640943e33495112e7944135e696c48
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2022-09-30 13:28:30 +0200

    + config

--- src/HomeServiceProvider.php
+++ src/HomeServiceProvider.php
@@ -15,6 +15,13 @@ class HomeServiceProvider extends ServiceProvider
         $this->loadJSONTranslationsFrom(__DIR__.'/../resources/lang');
  /*       $this->loadRoutesFrom(__DIR__.'/Routes/web.php'); */
         $this->loadViewsFrom(__DIR__.'/../resources/views','home'); 
+        
+          if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/openhab.php' => config_path('openhab.php'),
+                __DIR__.'/../config/zoneminder.php' => config_path('zoneminder.php'),            
+            ], 'config');
+          }
     }
 
 }
