b124ba1403d05eb53fa032f7885aa64224045b88
Klaus Dimde <klaus@dimde.info>
2024-01-04 10:51:58 +0100

    Controllers for floors and rooms

---
+++ src/Controllers/FloorController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Sunhill\Home\Controllers;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Blade;
+use Sunhill\Visual\Facades\Dialogs;
+use Sunhill\Visual\Test\TestDialogResponse;
+
+class FloorController extends Controller
+{
+    
+    public function index($floor)
+    {
+    }
+    
+}
